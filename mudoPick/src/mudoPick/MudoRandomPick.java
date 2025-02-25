3d9e35f6f04bf59ae7fe512b39a91c1ef8d22c6d
unknown <syc3261@gmail.com>
2025-02-26 00:17:28 +0900

    first commit

---
+++ mudoPick/src/mudoPick/MudoRandomPick.java
@@ -0,0 +1,28 @@
+package mudoPick;
+
+public class MudoRandomPick {
+    public static void main(String[] args) {
+
+        System.out.println("test");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
